deac14733d60c1520b0e4bbf96a99a0c38df5071
zhangfulin <1148112937@qq.com>
2021-11-24 04:21:26 +0800

    [init] webpack typescript

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+let i = 0;
+console.log(i + 1);
+console.log("ssss")
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: "/"
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: ['ts-loader'],
+        exclude: /node_modules/
+      },
+    ],
+  },
+  plugins:[
+      new HtmlWebpackPlugin({
+          template:"./src/index.html"
+      }),
+  ],
+
+  resolve: {
+    extensions: [".js", ".ts",]
+  }
+};
